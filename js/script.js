2d2071bac3fca2f8274c46ccee776d392db1100d
mdmansooralam <mdmansooralam@outlook.in>
2025-05-25 13:52:58 +0530

    upload local file to git

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector('.navbar')
+const logo = document.querySelector('.logo')
+
+window.addEventListener('scroll', ()=>{
+//     navbar.classList.remove('mt-xl-88')
+//     navbar.classList.add('bg-primary')
+})
